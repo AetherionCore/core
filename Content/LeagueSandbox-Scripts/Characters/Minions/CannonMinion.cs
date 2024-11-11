c6f7a8293ae7a6dc4d08839eb4579ebf3c06d6fb
Brian Oost <brianleon77@gmail.com>
2024-11-11 18:31:39 +0100

    [Add/Character] Placeholder Minions
    I have no idea if minions are supposed to do anything besides simple auto attacks. Currently it's flooding the server console with "Could not find script". It's something to look at later stage, since it's functional.

---
+++ Content/LeagueSandbox-Scripts/Characters/Minions/CannonMinion.cs
@@ -0,0 +1,13 @@
+using GameServerCore.Scripting.CSharp;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using LeagueSandbox.GameServer.GameObjects;
+using LeagueSandbox.GameServer.Scripting.CSharp;
+
+namespace Spells
+{
+    public class Blue_Minion_MechCannonBasicAttack : ISpellScript { public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata(); public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell) { } }
+    public class Blue_Minion_MechCannonBasicAttack2 : ISpellScript { public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata(); public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell) { } }
+    public class Red_Minion_MechCannonBasicAttack : ISpellScript { public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata(); public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell) { } }
+    public class Red_Minion_MechCannonBasicAttack2 : ISpellScript { public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata(); public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell) { } }
+}
