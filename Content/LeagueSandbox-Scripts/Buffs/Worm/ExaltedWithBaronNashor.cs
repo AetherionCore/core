1115c87e2f34a2993115fdab6053af964cb328c2
Brian Oost <brianleon77@gmail.com>
2024-11-28 18:53:15 +0100

    Finish baron buff damage

--- Content/LeagueSandbox-Scripts/Buffs/Worm/ExaltedWithBaronNashor.cs
+++ Content/LeagueSandbox-Scripts/Buffs/Worm/ExaltedWithBaronNashor.cs
@@ -25,48 +25,63 @@ namespace Buffs
 
         Buff thisBuff;
         Particle particle;
+
         private readonly Game _game;
+        public ExaltedWithBaronNashor() {}
+        public ExaltedWithBaronNashor(Game game)
+        {
+            _game = game;
+        }
 
         public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
         {
             thisBuff = buff;
 
-            if (_game == null)
-            {
-                // is null
-                return;
-            }
-
             if (unit is Champion champ)
             {
                 particle = AddParticleTarget(unit, unit, "nashor_rune_buf", unit, buff.Duration);
 
-                // Improved Recall is handled in ~
-                // Minion buffs TBA
+                /* 
+                    To be added:
 
+                    - Minion promote aura
 
-                /*
-                Grants up to 40 attack damage and ability power (scales with game time)
-                Improves Recall Recall:
-                Reduces channel time by 4 seconds
-                A successful recall restores 50% of your champion's maximum health / mana and grants +50% movement speed for 8 seconds.
-                https://leagueoflegends.fandom.com/wiki/Hand_of_Baron?oldid=2170918
+                    - Improved Recall
+
+                        Improved Recall data is missing:
+                        Should it be handled in:
+                            ~\Content\LeagueSandbox-Scripts\Characters\Global\Recall.cs
+                        By using:
+                            "if (owner.HasBuff("ExaltedWithBaronNashor"))" ?
+
+                        More information:
+                        - Grants up to 40 attack damage and ability power (scales with game time)
+                        - Improves Recall:
+                            - Reduces channel time by 4 seconds
+                            - A successful recall:
+                                - Restores 50% of your champion's maximum health/mana
+                                - Grants +50% movement speed for 8 seconds.
+
+                    Reference:
+                    https://leagueoflegends.fandom.com/wiki/Hand_of_Baron?oldid=2170918
                 */
 
-                float gameTime = _game.GameTime;
-                float bonusDamage = gameTime / 30;
-                //bonusDamage -= 15;
-                bonusDamage = Math.Min(bonusDamage, 20);
-                bonusDamage = Math.Max(bonusDamage, 40);
+                if (_game == null)
+                {
+                    LogDebug("_game is not initialized in 'ExaltedWithBaronNashor.cs'.");
+                    return; 
+                }
+                else
+                {
+                    float gameTime = _game.GameTime;
+                    float bonusDamage = gameTime / 30;
+                    bonusDamage -= 15;
+                    bonusDamage = Math.Min(bonusDamage, 20);
+                    bonusDamage = Math.Max(bonusDamage, 40);
 
-                StatsModifier.AttackDamage.FlatBonus += bonusDamage;
-                StatsModifier.AbilityPower.FlatBonus += bonusDamage;
-
-                /* example:
-                StatsModifier.ManaRegeneration.FlatBonus += 5 + unit.Stats.ManaPoints.Total * 0.05f;
-                StatsModifier.CooldownReduction.FlatBonus += 0.1f;
-                unit.AddStatModifier(StatsModifier);
-                */
+                    StatsModifier.AttackDamage.FlatBonus += bonusDamage;
+                    StatsModifier.AbilityPower.FlatBonus += bonusDamage;
+                }
             }
             else
             {
@@ -82,10 +97,9 @@ namespace Buffs
             The buff is lost upon a champion's death and cannot be transferred to another champion, 
             unlike the Crest of Insight Crest of Insight and Crest of Cinders Crest of Cinders, 
             which are not team-wide buffs.
-
-            https://leagueoflegends.fandom.com/wiki/Hand_of_Baron?oldid=2170918
             */
-            //thisBuff.DeactivateBuff();
+
+            thisBuff.DeactivateBuff();
         }
 
         public void OnDeactivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
@@ -96,7 +110,6 @@ namespace Buffs
 
         public void OnUpdate(float diff)
         {
-
         }
     }
 }
