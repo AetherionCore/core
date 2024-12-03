2d4d628d088bc6e1da792cac6f1dc3fc71edaba7
Brian Oost <brianleon77@gmail.com>
2024-12-03 18:43:48 +0100

    [Fix/Character] Volibear no longer pulls towers
    Closes: https://github.com/AetherionCore/Core/issues/31

--- src/Content/LeagueSandbox-Scripts/Characters/Volibear/Q.cs
+++ src/Content/LeagueSandbox-Scripts/Characters/Volibear/Q.cs
@@ -8,6 +8,7 @@ using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
 using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
 using LeagueSandbox.GameServer.GameObjects.SpellNS;
 using LeagueSandbox.GameServer.Scripting.CSharp;
+using System;
 using System.Numerics;
 using static LeagueSandbox.GameServer.API.ApiFunctionManager;
 
@@ -32,7 +33,7 @@ namespace Spells
             if (VolibearQAttack.Applied == 0)
             {
                 spell.CastInfo.Owner.PlayAnimation("Spell2", 0.5f, flags: AnimationFlags.Override);
-                CreateTimer(0.5f, () => { spell.CastInfo.Owner.StopAnimation("Spell1", fade: true); });
+                //CreateTimer(0.5f, () => { spell.CastInfo.Owner.StopAnimation("Spell1", fade: true); });
             }
         }
 
@@ -40,16 +41,17 @@ namespace Spells
         {
         }
 
-        public void OnSpellPreCast(ObjAIBase owner, Spell spell, AttackableUnit target, Vector2 start, Vector2 end)
+        /*public void OnSpellPreCast(ObjAIBase owner, Spell spell, AttackableUnit target, Vector2 start, Vector2 end)
         {
-            //var owner = spell.CastInfo.Owner as IChampion;
-            // ??
-            AddBuff("VoliQBuffSwag", 6.0f, 1, spell, owner, owner);
-
-        }
+            AddBuff("VolibearQ", 6.0f, 1, spell, owner, owner);
+        }*/
 
         public void OnSpellCast(Spell spell)
         {
+            var owner = spell.CastInfo.Owner;
+            PlayAnimation(owner, "Spell1");
+            owner.CancelAutoAttack(true);
+            AddBuff("VolibearQ", 4.0f, 1, spell, owner, owner);
         }
 
         public void OnSpellPostCast(Spell spell)
@@ -77,9 +79,9 @@ namespace Spells
     {
         public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
         {
-            TriggersSpellCasts = false,
-            NotSingleTargetSpell = true
-            // TODO
+            TriggersSpellCasts = true,
+            NotSingleTargetSpell = true,
+            IsDamagingSpell = true,
         };
 
         private Spell originspell;
@@ -102,11 +104,14 @@ namespace Spells
             if (Applied != 1)
             {
                 var unit = originspell.CastInfo.Targets[0].Unit;
-                var x = GetPointFromUnit(owner, -200);
-                unit.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_PHYSICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
-                var xy = unit as ObjAIBase;
-                xy.SetTargetUnit(null);
-                ForceMovement(unit, "Spell1", x, 500, 0, 20, 0);
+                if (!(unit is BaseTurret))
+                {
+                    var x = GetPointFromUnit(owner, -200);
+                    unit.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_PHYSICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
+                    var xy = unit as ObjAIBase;
+                    xy.SetTargetUnit(null);
+                    ForceMovement(unit, "Spell1", x, 500, 0, 20, 0);
+                }
                 Applied = 1;
                 //CreateTimer((float)6, () => { Applied = 1; });
             }
