879c2669b82471c43d485778af54bd8ab1cb0e8c
Brian Oost <brianleon77@gmail.com>
2024-12-06 13:23:43 +0100

    [Fix/Champion] Karthus R

--- src/Content/LeagueSandbox-Scripts/Characters/Karthus/R.cs
+++ src/Content/LeagueSandbox-Scripts/Characters/Karthus/R.cs
@@ -1,40 +1,69 @@
-using System.Linq;
-using GameServerCore;
 using GameServerCore.Enums;
 using static LeagueSandbox.GameServer.API.ApiFunctionManager;
 using LeagueSandbox.GameServer.Scripting.CSharp;
 using GameServerCore.Scripting.CSharp;
 using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using System.Numerics;
 
 namespace Spells
 {
-    public class FallenOne : ISpellScript
+    public class KarthusFallenOne : ISpellScript
     {
+        ObjAIBase Owner;
+        float TimeSinceLastTick = 0;
+        bool limiter = false;
+        float damage;
+
         public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
         {
-            // TODO
+            TriggersSpellCasts = true,
+            ChannelDuration = 3f,
         };
+
+        public void OnSpellPreCast(ObjAIBase owner, Spell spell, AttackableUnit target, Vector2 start, Vector2 end)
+        {
+            Owner = owner;
+        }
 
         public void OnSpellCast(Spell spell)
         {
             var owner = spell.CastInfo.Owner;
-            foreach (var enemyTarget in GetChampionsInRange(owner.Position, 20000, true)
-                .Where(x => x.Team == CustomConvert.GetEnemyTeam(owner.Team)))
+            var champions = GetChampionsInRange(owner.Position, 20000, true);
+            for (int i = 0; i < champions.Count; i++)
             {
-                AddParticleTarget(owner, enemyTarget, "KarthusFallenOne", enemyTarget);
+                if (champions[i].Team != owner.Team)
+                {
+                    AddParticleTarget(owner, champions[i], "Karthus_Base_R_Target.troy", champions[i], 3f);
+                }
             }
         }
 
         public void OnSpellPostCast(Spell spell)
         {
             var owner = spell.CastInfo.Owner;
-            var ap = owner.Stats.AbilityPower.Total;
-            var damage = 100 + spell.CastInfo.SpellLevel * 150 + ap * 0.6f;
-            foreach (var enemyTarget in GetChampionsInRange(owner.Position, 20000, true)
-                .Where(x => x.Team == CustomConvert.GetEnemyTeam(owner.Team)))
+            var ap = owner.Stats.AbilityPower.Total * 0.6f;
+            damage = 100 + spell.CastInfo.SpellLevel * 150 + ap;
+            TimeSinceLastTick = 0;
+            limiter = true;
+        }
+
+        public void OnUpdate(float diff)
+        {
+            TimeSinceLastTick += diff;
+            if (TimeSinceLastTick > 2700f && limiter == true && Owner != null)
             {
-                enemyTarget.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELL,
-                    false);
+                var champions = GetChampionsInRange(Owner.Position, 20000, true);
+                for (int i = 0; i < champions.Count; i++)
+                {
+                    if (champions[i].Team != Owner.Team && !champions[i].IsDead)
+                    {
+                        champions[i].TakeDamage(Owner, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELLAOE, false);
+                        AddParticleTarget(Owner, champions[i], "Karthus_Base_R_Explosion.troy", champions[i], 1f);
+                    }
+                }
+                limiter = false;
             }
         }
     }
