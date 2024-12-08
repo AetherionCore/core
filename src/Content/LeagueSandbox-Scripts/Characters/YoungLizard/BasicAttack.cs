62e1f3fdd01bfe6f6c1147ac84c01d7312d6884d
Brian Oost <brianleon77@gmail.com>
2024-12-08 20:58:27 +0100

    Young Lizard apply magic now too

--- src/Content/LeagueSandbox-Scripts/Characters/YoungLizard/BasicAttack.cs
+++ src/Content/LeagueSandbox-Scripts/Characters/YoungLizard/BasicAttack.cs
@@ -3,10 +3,40 @@ using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
 using LeagueSandbox.GameServer.GameObjects.SpellNS;
 using LeagueSandbox.GameServer.GameObjects;
 using LeagueSandbox.GameServer.Scripting.CSharp;
+using GameServerCore.Enums;
 
 namespace Spells
 {
-    public class YoungLizardBasicAttack : ISpellScript { public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata(); public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell) { } }
-    public class YoungLizardBasicAttack2 : ISpellScript { public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata(); public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell) { } }
-    public class YoungLizardBasicAttack3 : ISpellScript { public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata(); public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell) { } }
+    public class YoungLizardBasicAttack : ISpellScript
+    {
+        public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
+        {
+            TriggersSpellCasts = true,
+            IsDamagingSpell = true,
+            MissileParameters = new MissileParameters { Type = MissileType.Target }
+        };
+        public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell) { }
+    }
+
+    public class YoungLizardBasicAttack2 : ISpellScript
+    {
+        public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
+        {
+            TriggersSpellCasts = true,
+            IsDamagingSpell = true,
+            MissileParameters = new MissileParameters { Type = MissileType.Target }
+        };
+        public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell) { }
+    }
+
+    public class YoungLizardBasicAttack3 : ISpellScript
+    {
+        public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
+        {
+            TriggersSpellCasts = true,
+            IsDamagingSpell = true,
+            MissileParameters = new MissileParameters { Type = MissileType.Target }
+        };
+        public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell) { }
+    }
 }
