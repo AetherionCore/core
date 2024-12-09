7f68ed3a5b82f349be57dcfbd62a03eeb4c68664
Brian Oost <brianleon77@gmail.com>
2024-12-09 09:47:04 +0100

    Baron apply magic now too

---
+++ src/Content/LeagueSandbox-Scripts/Characters/Worm/BasicAttack.cs
@@ -0,0 +1,20 @@
+using GameServerCore.Scripting.CSharp;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using LeagueSandbox.GameServer.GameObjects;
+using LeagueSandbox.GameServer.Scripting.CSharp;
+using GameServerCore.Enums;
+
+namespace Spells
+{
+    public class WormBasicAttack : ISpellScript
+    {
+        public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
+        {
+            TriggersSpellCasts = true,
+            IsDamagingSpell = true,
+            MissileParameters = new MissileParameters { Type = MissileType.Target }
+        };
+        public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell) { }
+    }
+}
