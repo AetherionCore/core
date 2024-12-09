3656391afab81c3f6e2bccf18e3ebab23df6ea35
Brian Oost <brianleon77@gmail.com>
2024-12-09 09:47:33 +0100

    Dragon apply magic now too

--- src/Content/LeagueSandbox-Scripts/Characters/Dragon/BasicAttack.cs
+++ src/Content/LeagueSandbox-Scripts/Characters/Dragon/BasicAttack.cs
@@ -3,8 +3,18 @@ using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
 using LeagueSandbox.GameServer.GameObjects.SpellNS;
 using LeagueSandbox.GameServer.GameObjects;
 using LeagueSandbox.GameServer.Scripting.CSharp;
+using GameServerCore.Enums;
 
 namespace Spells
 {
-    public class DragonBasicAttack : ISpellScript { public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata(); public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell) { } }
+    public class DragonBasicAttack : ISpellScript
+    {
+        public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
+        {
+            TriggersSpellCasts = true,
+            IsDamagingSpell = true,
+            MissileParameters = new MissileParameters { Type = MissileType.Target }
+        };
+        public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell) { }
+    }
 }
