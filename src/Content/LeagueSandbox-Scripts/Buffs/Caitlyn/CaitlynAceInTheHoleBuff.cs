fd92601f4917d113014872d20de9bf894b0335a8
Brian Oost <brianleon77@gmail.com>
2024-12-12 10:15:10 +0100

    Added missing sfx for Caitlin R
    Closes:
    https://github.com/AetherionCore/core/issues/64

---
+++ src/Content/LeagueSandbox-Scripts/Buffs/Caitlyn/CaitlynAceInTheHoleBuff.cs
@@ -0,0 +1,38 @@
+﻿using GameServerCore.Enums;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+using LeagueSandbox.GameServer.GameObjects.StatsNS;
+using GameServerCore.Scripting.CSharp;
+using LeagueSandbox.GameServer.Scripting.CSharp;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using LeaguePackets.Game.Events;
+
+namespace Buffs
+{
+    internal class CaitlynAceintheHole : IBuffGameScript
+    {
+        public BuffScriptMetaData BuffMetaData { get; set; } = new BuffScriptMetaData
+        {
+            BuffType = BuffType.SLOW,
+            BuffAddType = BuffAddType.REPLACE_EXISTING
+        };
+
+        public StatsModifier StatsModifier { get; private set; } = new StatsModifier();
+
+        Particle p;
+        Particle p2;
+
+        public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
+        {
+            p = AddParticleTarget(ownerSpell.CastInfo.Owner, ownerSpell.CastInfo.Owner, "caitlyn_laser_beam_01.troy", unit, buff.Duration);
+            p2 = AddParticleTarget(ownerSpell.CastInfo.Owner, unit, "caitlyn_ace_target_indicator.troy", unit, buff.Duration);
+        }
+
+        public void OnDeactivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
+        {
+            RemoveParticle(p);
+            RemoveParticle(p2);
+        }
+    }
+}
--- src/Content/LeagueSandbox-Scripts/Characters/Caitlyn/R.cs
+++ src/Content/LeagueSandbox-Scripts/Characters/Caitlyn/R.cs
@@ -1,66 +1,41 @@
 using GameServerCore.Enums;
 using LeagueSandbox.GameServer.Scripting.CSharp;
-using GameServerCore.Scripting.CSharp;
-using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
-using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
-using LeagueSandbox.GameServer.GameObjects.SpellNS;
-using LeagueSandbox.GameServer.GameObjects.SpellNS.Missile;
-using LeagueSandbox.GameServer.API;
-using LeagueSandbox.GameServer.GameObjects.SpellNS.Sector;
 using System.Numerics;
+using LeagueSandbox.GameServer.API;
 using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+using GameServerCore.Scripting.CSharp;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.SpellNS.Missile;
+using LeagueSandbox.GameServer.GameObjects.SpellNS.Sector;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
 
 namespace Spells
 {
     public class CaitlynAceintheHole : ISpellScript
     {
+        ObjAIBase Owner;
+        AttackableUnit Target;
         public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
         {
             ChannelDuration = 1.25f,
             TriggersSpellCasts = true
-            // TODO
         };
-
-        public void OnActivate(ObjAIBase owner, Spell spell)
-        {
-        }
-
-        public void OnDeactivate(ObjAIBase owner, Spell spell)
-        {
-        }
-
-        internal static AttackableUnit targ;
 
         public void OnSpellPreCast(ObjAIBase owner, Spell spell, AttackableUnit target, Vector2 start, Vector2 end)
         {
-            targ = target;
-            owner.Stats.SetActionState(ActionState.CAN_MOVE, false);
-            CreateTimer(1.5f, () => { owner.Stats.SetActionState(ActionState.CAN_MOVE, true); });
+            Owner = owner;
+            Target = target as Champion;
         }
 
         public void OnSpellCast(Spell spell)
         {
-        }
-
-        public void OnSpellPostCast(Spell spell)
-        {
-        }
-
-        public void OnSpellChannel(Spell spell)
-        {
-        }
-
-        public void OnSpellChannelCancel(Spell spell, ChannelingStopSource source)
-        {
+            AddBuff("CaitlynAceintheHole", 1.5f, 1, spell, Target, Owner);
         }
 
         public void OnSpellPostChannel(Spell spell)
         {
-            SpellCast(spell.CastInfo.Owner, 0, SpellSlotType.ExtraSlots, targ.Position, targ.Position, true, Vector2.Zero);
-        }
-
-        public void OnUpdate(float diff)
-        {
+            SpellCast(Owner, 0, SpellSlotType.ExtraSlots, true, Target, Vector2.Zero);
         }
     }
 
@@ -71,9 +46,8 @@ namespace Spells
             TriggersSpellCasts = true,
             MissileParameters = new MissileParameters
             {
-                Type = MissileType.Circle
+                Type = MissileType.Target
             }
-            // TODO
         };
 
         public void OnActivate(ObjAIBase owner, Spell spell)
@@ -81,47 +55,19 @@ namespace Spells
             ApiEventManager.OnSpellHit.AddListener(this, spell, TargetExecute, false);
         }
 
-        public void OnDeactivate(ObjAIBase owner, Spell spell)
-        {
-        }
-
-        public void OnSpellPreCast(ObjAIBase owner, Spell spell, AttackableUnit target, Vector2 start, Vector2 end)
-        {
-        }
-
-        public void OnSpellCast(Spell spell)
-        {
-        }
-
-        public void OnSpellPostCast(Spell spell)
-        {
-        }
-
         public void TargetExecute(Spell spell, AttackableUnit target, SpellMissile missile, SpellSector sector)
         {
-            if (target != null && !target.IsDead && target is Champion)
+            var owner = spell.CastInfo.Owner;
+            var spellLevel = owner.GetSpell("CaitlynAceintheHole").CastInfo.SpellLevel;
+            if (target != null && !target.IsDead)
             {
-                // 250/475/700
-                var damage = 250 + spell.CastInfo.Owner.Stats.AttackDamage.Total * 2;
-                target.TakeDamage(spell.CastInfo.Owner, damage, DamageType.DAMAGE_TYPE_PHYSICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
-                missile.SetToRemove();
+                var ADratio = owner.Stats.AttackDamage.FlatBonus * 2f;
+                var damage = 25f + (225f * spellLevel) + ADratio;
+
+                target.TakeDamage(owner, damage, DamageType.DAMAGE_TYPE_PHYSICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
+                AddParticleTarget(owner, target, "caitlyn_ace_tar.troy", target, lifetime: 1f);
             }
-        }
-
-        public void OnSpellChannel(Spell spell)
-        {
-        }
-
-        public void OnSpellChannelCancel(Spell spell, ChannelingStopSource source)
-        {
-        }
-
-        public void OnSpellPostChannel(Spell spell)
-        {
-        }
-
-        public void OnUpdate(float diff)
-        {
+            missile.SetToRemove();
         }
     }
 }
