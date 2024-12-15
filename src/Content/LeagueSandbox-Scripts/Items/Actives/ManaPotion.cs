0261f38e1ecaec819d0c85769be01559ded099f5
Brian Oost <brianleon77@gmail.com>
2024-12-15 22:14:05 +0100

    Update ManaPotion.cs

--- src/Content/LeagueSandbox-Scripts/Items/Actives/ManaPotion.cs
+++ src/Content/LeagueSandbox-Scripts/Items/Actives/ManaPotion.cs
@@ -5,6 +5,9 @@ using GameServerCore.Scripting.CSharp;
 using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
 using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
 using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using LeagueSandbox.GameServer.API;
+using GameServerCore.Enums;
+using GameServerLib.GameObjects.AttackableUnits;
 
 namespace ItemSpells
 {
@@ -14,6 +17,46 @@ namespace ItemSpells
         {
             // TODO
         };
+        float lastWarning = 0;
+        Spell manaPotion;
+
+        public void OnActivate(ObjAIBase owner, Spell spell)
+        {
+            manaPotion = spell;
+            ApiEventManager.OnAllowUseItem.AddListener(this, owner, UseItem);
+        }
+
+        private bool CanWarn()
+        {
+            var timeNow = GameTime / 1000f;
+            if (timeNow - lastWarning >= 4) // 4 seconds passed since last warning
+            {
+                lastWarning = timeNow;
+                return true;
+            }
+            return false;
+        }
+
+        private bool UseItem(ObjAIBase unit, Spell spell, byte slot)
+        {
+            var champ = unit as Champion;
+            if (unit.IsDead)
+            {
+                if (CanWarn())
+                    SendWarningPopup(champ, "Dead", champ.ClientId);
+                return false;
+            }
+            if (spell.SpellName == manaPotion.SpellName)
+            {
+                if (unit.Stats.CurrentMana >= unit.Stats.ManaPoints.Total)
+                {
+                    if (CanWarn())
+                        SendWarningPopup(champ, "Full MP", champ.ClientId, FloatTextType.Absorbed);
+                    return false;
+                }
+            }
+            return true;
+        }
 
         public void OnSpellPreCast(ObjAIBase owner, Spell spell, AttackableUnit target, Vector2 start, Vector2 end)
         {
