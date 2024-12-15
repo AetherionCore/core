c744b85be7f0eeee5d2246fb483d3f1005dc55e2
Brian Oost <brianleon77@gmail.com>
2024-12-15 22:18:13 +0100

    Update SummonerTeleport.cs

--- src/Content/LeagueSandbox-Scripts/Characters/Global/SummonerTeleport.cs
+++ src/Content/LeagueSandbox-Scripts/Characters/Global/SummonerTeleport.cs
@@ -6,7 +6,6 @@ using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
 using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
 using LeagueSandbox.GameServer.GameObjects.SpellNS;
 using GameServerCore.Enums;
-using LeagueSandbox.GameServer.GameObjects;
 
 namespace Spells
 {
@@ -18,27 +17,63 @@ namespace Spells
         {
             CastingBreaksStealth = false,
             ChannelDuration = 4.0f,
-            TriggersSpellCasts = true,
+            TriggersSpellCasts = false,
             NotSingleTargetSpell = false
         };
+
+        void ProtectTarget(bool protect)
+        {
+            if (protect)
+            {
+                Target.StopMovement();
+                if (Target is ObjAIBase minion)
+                    minion.SetTargetUnit(null, true);
+            }
+            Target.SetStatus(StatusFlags.CanAttack, !protect);
+            Target.SetStatus(StatusFlags.CanMove, !protect);
+            Target.SetStatus(StatusFlags.CanCast, !protect);
+            Target.SetStatus(StatusFlags.Invulnerable, protect);
+            Target.SetStatus(StatusFlags.Immovable, protect);
+            LogInfo($"Protecting Target {Target.CharData.Name}");
+        }
+
+        void ProtectCaster(bool protect)
+        {
+            if (protect)
+            {
+                Owner.StopMovement();
+                Owner.SetTargetUnit(null, true);
+            }
+            Owner.SetStatus(StatusFlags.CanAttack, !protect);
+            Owner.SetStatus(StatusFlags.CanMove, !protect);
+            Owner.SetStatus(StatusFlags.CanMoveEver, !protect);
+            Owner.SetStatus(StatusFlags.CanCast, !protect);
+            LogInfo($"Protecting Caster {protect}");
+        }
 
         public void OnSpellChannel(Spell spell)
         {
             Target = spell.CastInfo.Targets[0].Unit;
             Owner = spell.CastInfo.Owner as Champion;
-            var p101 = AddParticleTarget(Owner, Owner, "Summoner_Teleport_purple.troy", Owner);
-            var p102 = AddParticleTarget(Owner, Owner, "teleport.troy", Owner);
-            var p103 = AddParticleTarget(Owner, Target, "Teleport_target.troy", Target);
-            var p104 = AddParticleTarget(Owner, Target, "Scroll_Teleport.troy", Target);
-            if (Target is LaneMinion or Minion or Monster)
+            var p101 = AddParticleTarget(Owner, Owner, "Summoner_Teleport_purple.troy", Owner, 4f);
+            //var p102 = AddParticleTarget(Owner, Owner, "Summoner_Teleport.troy", Target, 4f);
+            var p104 = AddParticle(Owner, Target, "Summoner_Teleport.troy", Target.Position, 4f);
+            var p103 = AddParticleTarget(Owner, Target, "Summoner_Cast.troy", Target, 4f);
+
+            ProtectCaster(true);
+            if (Target is Minion)
             {
-                LogInfo($"Teleporting to Minion {Target.CharData.Name}");
-                AddBuff("TeleportBuff", 4f, 1, spell, Target, null);
+                ProtectTarget(true);
             }
         }
 
         public void OnSpellChannelCancel(Spell spell, ChannelingStopSource reason)
         {
+            ProtectCaster(false);
+            if (Target is Minion)
+            {
+                ProtectTarget(false);
+            }
         }
 
         public void OnSpellPostChannel(Spell spell)
@@ -46,11 +81,16 @@ namespace Spells
             Target = spell.CastInfo.Targets[0].Unit;
             Owner = spell.CastInfo.Owner as Champion;
             TeleportTo(Owner, Target.Position.X, Target.Position.Y);
-            AddParticleTarget(Owner, Owner, "TeleportArrive", Owner, flags: 0);
-            var p201 = AddParticleTarget(Owner, Owner, "Summoner_TeleportArrive_purple.troy", Owner);
-            var p202 = AddParticleTarget(Owner, Owner, "teleportarrive.troy", Owner);
-            var p203 = AddParticleTarget(Owner, Owner, "scroll_teleportarrive.troy", Owner);
-            RemoveBuff(Target, "TeleportBuff");
+            //AddParticleTarget(Owner, Owner, "TeleportArrive", Owner, flags: 0);
+            var p201 = AddParticleTarget(Owner, Owner, "summoner_teleportarrive.troy", Target, 1f);
+            //var p202 = AddParticleTarget(Owner, Owner, "teleportarrive.troy", Owner);
+            //var p203 = AddParticleTarget(Owner, Owner, "scroll_teleportarrive.troy", Owner);
+
+            ProtectCaster(false);
+            if (Target is Minion)
+            {
+                ProtectTarget(false);
+            }
         }
     }
 }
