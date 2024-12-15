bd5aa9ec7a4ffbb441131b22a8d9da736e3246ae
Brian Oost <brianleon77@gmail.com>
2024-12-15 22:11:32 +0100

    Lesser Wraith enabled magic attacks

--- src/Content/LeagueSandbox-Scripts/Characters/LesserWraith/BasicAttack.cs
+++ src/Content/LeagueSandbox-Scripts/Characters/LesserWraith/BasicAttack.cs
@@ -3,10 +3,35 @@ using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
 using LeagueSandbox.GameServer.GameObjects.SpellNS;
 using LeagueSandbox.GameServer.GameObjects;
 using LeagueSandbox.GameServer.Scripting.CSharp;
+using GameServerCore.Enums;
 
 namespace Spells
 {
-    public class LesserWraithBasicAttack : ISpellScript { public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata(); public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell) { } }
-    public class LesserWraithBasicAttack2 : ISpellScript { public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata(); public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell) { } }
-    public class LesserWraithBasicAttack3 : ISpellScript { public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata(); public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell) { } }
+    public class LesserWraithBasicAttack : ISpellScript
+    {
+        public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
+        {
+            TriggersSpellCasts = true,
+            IsDamagingSpell = true,
+            MissileParameters = new MissileParameters { Type = MissileType.Target }
+        }; public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell) { }
+    }
+    public class LesserWraithBasicAttack2 : ISpellScript
+    {
+        public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
+        {
+            TriggersSpellCasts = true,
+            IsDamagingSpell = true,
+            MissileParameters = new MissileParameters { Type = MissileType.Target }
+        }; public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell) { }
+    }
+    public class LesserWraithBasicAttack3 : ISpellScript
+    {
+        public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
+        {
+            TriggersSpellCasts = true,
+            IsDamagingSpell = true,
+            MissileParameters = new MissileParameters { Type = MissileType.Target }
+        }; public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell) { }
+    }
 }
