59ce20c9473d100f217a9c46dfba290da2cf1738
Brian Oost <brianleon77@gmail.com>
2024-12-15 22:15:19 +0100

    Added Grievous wounds system

---
+++ src/Content/LeagueSandbox-Scripts/Buffs/Global/GrievousWounds.cs
@@ -0,0 +1,40 @@
+﻿using GameServerCore.Enums;
+using GameServerCore.Scripting.CSharp;
+using LeagueSandbox.GameServer.GameObjects;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using LeagueSandbox.GameServer.GameObjects.StatsNS;
+using LeagueSandbox.GameServer.Scripting.CSharp;
+using System.Numerics;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+using GameServerCore;
+
+namespace Buffs
+{
+    internal class GrievousWounds : IBuffGameScript
+    {
+        public BuffScriptMetaData BuffMetaData { get; set; } = new BuffScriptMetaData
+        {
+            BuffType = BuffType.POISON,
+            BuffAddType = BuffAddType.RENEW_EXISTING,
+            MaxStacks = 1
+        };
+
+        public StatsModifier StatsModifier { get; private set; } = new StatsModifier();
+
+
+        public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
+        {
+            StatsModifier.HealthRegeneration.PercentBonus = -0.5f;
+
+            unit.AddStatModifier(StatsModifier);
+        }
+
+        public void OnDeactivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
+        {
+            StatsModifier.HealthRegeneration.PercentBonus = -0.5f;
+            unit.RemoveStatModifier(StatsModifier);
+        }
+    }
+}
